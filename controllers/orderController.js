077c1f841229e904399072e15e332a859f4ebd12
Amit rawat <157109376+amit-rawat09@users.noreply.github.com>
2025-04-05 22:06:53 +0530

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order from frontend
 const placeorder = async (req, res) => {
 
-    const frontend_url = 'https://yum-express.vercel.app'
+    const frontend_url = 'https://localhost:5174'
 
     try {
         const newOrder = new orderModel({
